2578bc408a202bcaf77f07ff7434b5ef38dd6cab
olawalejuwonm <olawalejuwon@gmail.com>
2020-04-08 16:56:43 +0100

    Used Let For Declaration

--- HngJavascriptTask2.js
+++ HngJavascriptTask2.js
@@ -1,32 +1,30 @@
-var data = [{principal: 2500, time: 1.8}, {principal: 1000, time: 5},
- {principal: 3000, time: 1},
-{principal: 2000, time: 3}]
+let data = [{principal: 2500, time: 1.8}, {principal: 1000, time: 5},
+        {principal: 3000, time: 1},
+        {principal: 2000, time: 3}];
 function interestCalculator(arr) {
-	var interestData = []
-	for (indx in data){
-		// var rate = 3
-		// var interest = (arr[indx].principal * arr[indx].time * rate )/100
+    let interestData = [];
+     for (indx in data){
 		if (arr[indx].principal >= 2500 && arr[indx].time > 1 && arr[indx].time <3 ) {
-			var rate = 3
-			var interest = (arr[indx].principal * arr[indx].time * rate )/100
+			let rate = 3
+			let interest = (arr[indx].principal * arr[indx].time * rate )/100
 			interestData.push({principal: arr[indx].principal,
 		rate: rate, time: arr[indx].time, interest: interest})
 		}
 		else if (arr[indx].principal >= 2500 && arr[indx].time >=  3) {
-			var rate = 4
-			var interest = (arr[indx].principal * arr[indx].time * rate )/100
+			let rate = 4
+			let interest = (arr[indx].principal * arr[indx].time * rate )/100
 			interestData.push({principal: arr[indx].principal,
 		rate: rate, time: arr[indx].time, interest: interest})
 		}
 		else if (arr[indx].principal <= 2500 | arr[indx].time <= 1 ) {
-			var rate = 2
-			var interest = (arr[indx].principal * arr[indx].time * rate )/100
+			let rate = 2
+			let interest = (arr[indx].principal * arr[indx].time * rate )/100
 			interestData.push({principal: arr[indx].principal,
 		rate: rate, time: arr[indx].time, interest: interest})
 		}
 		else {
-			var rate = 1
-			var interest = (arr[indx].principal * arr[indx].time * rate )/100
+			let rate = 1
+			let interest = (arr[indx].principal * arr[indx].time * rate )/100
 			interestData.push({principal: arr[indx].principal,
 		rate: rate, time: arr[indx].time, interest: interest})
 		}
@@ -39,7 +37,4 @@ function interestCalculator(arr) {
 	
 	
 }
-// for (key in data){
-// 	console.log(data[key].principal)
-// }
 interestCalculator(data)
